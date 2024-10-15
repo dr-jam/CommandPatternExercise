00a4f4c1e7fc2e4d3e0effadfc4939338ff80302
Josh McCoy <joshua.allen.mccoy@gmail.com>
2024-10-15 00:39:17 -0700

    removed unity materials

--- Captain/Assets/Scripts/ICaptainCommand.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-namespace Captain.Command
-{
-    public interface ICaptainCommand
-    {
-        void Execute(GameObject gameObject);
-    }
-}
--- Captain/Assets/Scripts/IPirateCommand.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-namespace Captain.Command
-{
-    public interface IPirateCommand
-    {
-        bool Execute(GameObject pirate, Object productPrefab);
-    }
-}
